63000dac1aa9bf4a1bc4a192766477e64d698c60
Ednilson <ednilsoncastanheira53@gmail.com>
2025-03-23 23:49:14 +0000

    removed the static code option

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/ednilson',
-  assetPrefix: '/ednilson/',
-  images: {
-    unoptimized: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
