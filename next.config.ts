0eb2866f8a9b42d709c42f3f88112007885e3a15
Ednilson <ednilsoncastanheira53@gmail.com>
2025-03-24 00:07:23 +0000

    fixing the deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/ednilson',
-  assetPrefix: '/ednilson/',
-  images: {
-    unoptimized: true,
-  }
+ 
 };
 
 export default nextConfig;
